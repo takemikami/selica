aaf691e0215b83f67d240672df11099adea276e7
Mikami, Takeshi <takeshi.mikami@gmail.com>
2019-06-08 14:19:11 +0900

    add cosine similarity

--- python/pyselica/ml/feature.py
+++ python/pyselica/ml/feature.py
@@ -1,7 +1,12 @@
 from pyspark import keyword_only
+from pyspark.ml import Estimator, Transformer, Model
 from pyspark.ml.wrapper import JavaTransformer
 from pyspark.ml.util import JavaMLReadable, JavaMLWritable
 from pyspark.ml.param.shared import HasInputCol, HasOutputCol
+from pyspark.sql import functions as F
+from pyspark.sql import Window
+from pyspark.sql.types import FloatType, ArrayType
+import numpy as np
 
 
 class JapaneseTokenizer(JavaTransformer, HasInputCol, HasOutputCol, JavaMLReadable, JavaMLWritable):
@@ -17,3 +22,37 @@ class JapaneseTokenizer(JavaTransformer, HasInputCol, HasOutputCol, JavaMLReadab
     def setParams(self, inputCol=None, outputCol=None):
         kwargs = self._input_kwargs
         return self._set(**kwargs)
+
+
+class CosineSimilarityModel(Model):
+    def __init__(self, vectorCol, similarityColPrefix, rankColPrefix, baseDataset):
+        super(CosineSimilarityModel, self).__init__()
+        self._vectorCol = vectorCol
+        self._similarityColPrefix = similarityColPrefix
+        self._rankColPrefix = rankColPrefix
+        self._baseDataset = baseDataset
+
+    def mostSimilar(self, vecs, k=None):
+        cos_sim_udf = F.udf(lambda a, b: float(abs(np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b)))),
+                            FloatType())
+        df = self._baseDataset
+        for col_vec in zip(self._vectorCol, vecs):
+            ary = F.array([F.lit(v) for v in col_vec[1]])
+            sim_colname = "{}{}".format(self._similarityColPrefix, col_vec[0])
+            rank_colname = "{}{}".format(self._rankColPrefix, col_vec[0])
+            df = df.withColumn(sim_colname, cos_sim_udf(col_vec[0], ary)) \
+                .withColumn(rank_colname, F.row_number().over(Window.orderBy(F.desc(sim_colname))))
+            if k is not None:
+                df = df.where(F.col(rank_colname) <= k)
+        return df
+
+
+class CosineSimilarity(Estimator):
+    def __init__(self, vectorCol=["vector"], similarityColPrefix="sim_", rankColPrefix="rank_"):
+        super(CosineSimilarity, self).__init__()
+        self._vectorCol = vectorCol
+        self._similarityColPrefix = similarityColPrefix
+        self._rankColPrefix = rankColPrefix
+
+    def _fit(self, dataset):
+        return CosineSimilarityModel(self._vectorCol, self._similarityColPrefix, self._rankColPrefix, dataset)
--- python/tests/test_ml_feature.py
+++ python/tests/test_ml_feature.py
@@ -1,6 +1,8 @@
 import unittest
+
+from pyspark.ml.linalg import Vectors
 from pyspark.sql import SparkSession, SQLContext
-from pyselica.ml.feature import JapaneseTokenizer
+from pyselica.ml.feature import JapaneseTokenizer, CosineSimilarity
 
 
 class TestCases(unittest.TestCase):
@@ -10,11 +12,46 @@ class TestCases(unittest.TestCase):
 
     def test_japanese_tokenizer(self):
         tokenizer = JapaneseTokenizer(inputCol="sentence", outputCol="words")
-
         sqlContext = SQLContext(self.spark)
         df = sqlContext.createDataFrame([(1, "すもももももももものうち")], ["id", "sentence"])
+
         tokenized = tokenizer.transform(df)
 
         pdf = tokenized.toPandas()
         self.assertTrue('words' in pdf.columns)
         self.assertEquals("すもももももももものうち", ''.join(pdf[pdf['id'] == 1].words.values[0]))
+
+    def test_cosine_similarity(self):
+        sqlContext = SQLContext(self.spark)
+        df = sqlContext.createDataFrame([
+            (1, Vectors.dense([1.0, 1.0])),
+            (2, Vectors.dense([1.0, 0.9])),
+            (3, Vectors.dense([1.0, 0.5])),
+            (4, Vectors.dense([0.5, 1.0])),
+        ], ["id", "vector"])
+
+        cossim = CosineSimilarity().fit(df)
+        rtn = cossim.mostSimilar([Vectors.dense([1.0, 1.0])])
+
+        pdf = rtn.toPandas()
+        self.assertTrue('sim_vector' in pdf.columns)
+        self.assertTrue('rank_vector' in pdf.columns)
+        self.assertEquals(1, pdf[pdf['rank_vector'] == 1].id.values[0])
+
+    def test_cosine_similarity_multi(self):
+        sqlContext = SQLContext(self.spark)
+        df = sqlContext.createDataFrame([
+            (1, Vectors.dense([1.0, 1.0]), Vectors.dense([1.0, 0.9])),
+            (2, Vectors.dense([1.0, 0.9]), Vectors.dense([0.5, 1.0])),
+            (3, Vectors.dense([1.0, 0.5]), Vectors.dense([1.0, 0.5])),
+            (4, Vectors.dense([0.5, 1.0]), Vectors.dense([1.0, 1.0])),
+        ], ["id", "vector1", "vector2"])
+
+        cossim = CosineSimilarity(vectorCol=["vector1", "vector2"]).fit(df)
+        rtn = cossim.mostSimilar([Vectors.dense([1.0, 1.0]), Vectors.dense([1.0, 1.0])])
+        
+        pdf = rtn.toPandas()
+        self.assertTrue('sim_vector1' in pdf.columns)
+        self.assertTrue('rank_vector1' in pdf.columns)
+        self.assertEquals(1, pdf[pdf['rank_vector1'] == 1].id.values[0])
+        self.assertEquals(4, pdf[pdf['rank_vector2'] == 1].id.values[0])
